ce08213806758fea0f61f56390c232a9a16ece04
Donald Abua <abuadonald@gmail.com>
2022-02-24 22:11:21 +0100

    homepage

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,19 +1,22 @@
 import React from "react";
-
-// Assets
-import imgg from "../assets/images/avatar.jpg";
+import Project from "./Project";
+import arrowRight from "../assets/icons/arrow-right.png";
 
 export default function Portfolio() {
   return (
     <section className="portfolio">
       <div>
         <h2>Portfolio</h2>
-        <div className="project">
-          <img src={imgg} alt=""></img>
-          <h3>Project Name</h3>
-          <div className="overlay"></div>
-        </div>
-        <button>My Repository</button>
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <button>
+          My Repository
+          <span>
+            <img src={arrowRight} alt=""></img>
+          </span>
+        </button>
       </div>
     </section>
   );
---
+++ src/components/Project.js
@@ -0,0 +1,16 @@
+import React from "react";
+// Assets
+import imgg from "../assets/images/avatar.jpg";
+
+export default function Project() {
+  return (
+    <div className="project">
+      <a href="https://www.google.com" target="_blank" rel="noreferrer">
+        {" "}
+      </a>
+      <h3>Project Name</h3>
+      <img src={imgg} alt=""></img>
+      <div className="overlay"></div>
+    </div>
+  );
+}
