b0145ec687f5619bd08e6604001789779795c81a
Donald Abua <abuadonald@gmail.com>
2022-03-01 22:44:31 +0100

    commit

--- src/components/home/Portfolio.jsx
+++ src/components/home/Portfolio.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Project from "./Project";
 
 // Assets
 import arrowRight from "../../assets/icons/arrow-right.png";
+import imgg from "../../assets/images/avatar.jpg";
 
 export default function Portfolio() {
+  const projects = [
+    {
+      title: "Space Tourism",
+      img: imgg,
+      link: "https://space-vegan.netlify.app",
+    },
+    {
+      title: "To-Do",
+      img: imgg,
+      link: "hhttps://todo-vegan.netlify.app",
+    },
+    {
+      title: "Fund",
+      img: imgg,
+      link: "https://funds-vegan.netlify.app",
+    },
+    {
+      title: "Lets Roll",
+      img: imgg,
+      link: "https://lets-roll-vegan.netlify.app",
+    },
+  ];
+
+  const renderProjects = projects.map((pro) => (
+    <Project title={pro.title} img={pro.img} link={pro.link} />
+  ));
+
   return (
     <section className='portfolio'>
       <h2 id='port'>Portfolio</h2>
       <div>
-        <div className='projects grid'>
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-        </div>
+        <div className='projects grid'>{renderProjects}</div>
         <button className='repo-btn'>
           <a href='https://github.com/millidavitti' target='blank'>
             {" "}
--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -2,15 +2,16 @@ import React from "react";
 // Assets
 import imgg from "../../assets/images/avatar.jpg";
 
-export default function Project() {
+export default function Project(props) {
+  const { title, img, link } = props;
   return (
-    <div className="project">
-      <a href="https://www.google.com" target="_blank" rel="noreferrer">
+    <div className='project'>
+      <a href={link} target='_blank' rel='noreferrer'>
         {" "}
       </a>
-      <h3>Project Name</h3>
-      <img src={imgg} alt=""></img>
-      <div className="overlay"></div>
+      <h3>{title}</h3>
+      <img src={img} alt=''></img>
+      <div className='overlay'></div>
     </div>
   );
 }
