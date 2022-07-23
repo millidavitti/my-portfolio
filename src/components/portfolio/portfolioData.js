81444464f0f3649d247336a31db96b03d1b4d14e
Donald Abua <abuadonald@gmail.com>
2022-07-23 11:48:28 +0100

    commit

--- src/components/portfolio/portfolioData.js
+++ src/components/portfolio/portfolioData.js
@@ -6,69 +6,22 @@ import fork from "../../assets/images/fork.png";
 import torch from "../../assets/images/torch.png";
 const portfolioData = [
 	{
-		id: 1,
-		title: "Space Tourism",
-		img: space,
+		id: 6,
+		title: "Torch Blog",
+		img: torch,
+		alt: "torch-blog-screenshot",
 		text: (
 			<p>
-				Used optional chaining (ES6 syntax) to add event listeners to DOM
-				elements that weren't displayed on the current page.
-				<br />
-				<br />
-				Displayed information dynamically using a local JSON data file contained
-				to cut down on the amount of html written.
-				<br />
-				<br /> Made use of object destructuring while fetching data from the
-				JSON data file to minimise the creation of redundant variables.
-				<br />
-				<br /> Figma design files were being used as a guide to laying out the
-				website.
-				<br />
-				<br /> The desktop navigation was implemented in JavaScript to save
-				myself the burden of copying/pasting the html on every page created.
-				<br />
-				<br />
-				That way, the menu appears on new pages automatically. The desktop
-				navigation was implemented using ES6 classes and private fields
-			</p>
-		),
-	},
-	{
-		id: 2,
-		title: "Funds",
-		img: funds,
-		text: (
-			<p>
-				A simple bank dashboard with transaction history Implementation of the
-				transfer, request loan, close account feature were implemented with data
-				transformation array methods like map, filter and reduce.
-				<br />
-				<br />
-				Utilised the Internationalisation API to format date, time and greetings
-				based on the geolocation of the client's browser.
-				<br />
-				<br />
-				This improved accessibility of the web application. Implemented an
-				automated log out feature that logged the user out after a set time with
-				the setTimeout function (pun intended)
-			</p>
-		),
-	},
-	{
-		id: 3,
-		title: "To-do",
-		img: todo,
-		text: <p>This simple but useful project was implemented using React.</p>,
-	},
-	{
-		id: 4,
-		title: "Lets Roll",
-		img: roll,
-		text: (
-			<p>
-				Vanilla Javascript was used to implement the games functionality.
-				<br /> <br /> Made use of Object-oriented programming paradigm to build
-				an develop this game.
+				Torch is an indispensable blog & magazine theme with a clean, modern
+				design suitable for everyone who wants to share their stories about
+				today’s ever-changing technology, the latest breaking news or the
+				hottest products.
+				<br></br>
+				<br></br>
+				<p>
+					<strong> Tool Chain :</strong> ReactJS, NextJS, Strapi, Apollo Client,
+					Heroku
+				</p>
 			</p>
 		),
 	},
@@ -99,17 +52,69 @@ const portfolioData = [
 		),
 	},
 	{
-		id: 6,
-		title: "Torch Blog",
-		img: torch,
-		alt: "torch-blog-screenshot",
+		id: 4,
+		title: "Lets Roll",
+		img: roll,
 		text: (
 			<p>
-				Torch is an indispensable blog & magazine theme with a clean, modern
-				design suitable for everyone who wants to share their stories about
-				today’s ever-changing technology, the latest breaking news or the
-				hottest products. Tool Chain: ReactJS, NextJS, Strapi, Apollo Client,
-				Heroku
+				Vanilla Javascript was used to implement the games functionality.
+				<br /> <br /> Made use of Object-oriented programming paradigm to build
+				an develop this game.
+			</p>
+		),
+	},
+	{
+		id: 3,
+		title: "To-do",
+		img: todo,
+		text: <p>This simple but useful project was implemented using React.</p>,
+	},
+	{
+		id: 2,
+		title: "Funds",
+		img: funds,
+		text: (
+			<p>
+				A simple bank dashboard with transaction history Implementation of the
+				transfer, request loan, close account feature were implemented with data
+				transformation array methods like map, filter and reduce.
+				<br />
+				<br />
+				Utilised the Internationalisation API to format date, time and greetings
+				based on the geolocation of the client's browser.
+				<br />
+				<br />
+				This improved accessibility of the web application. Implemented an
+				automated log out feature that logged the user out after a set time with
+				the setTimeout function (pun intended)
+			</p>
+		),
+	},
+	{
+		id: 1,
+		title: "Space Tourism",
+		img: space,
+		text: (
+			<p>
+				Used optional chaining (ES6 syntax) to add event listeners to DOM
+				elements that weren't displayed on the current page.
+				<br />
+				<br />
+				Displayed information dynamically using a local JSON data file contained
+				to cut down on the amount of html written.
+				<br />
+				<br /> Made use of object destructuring while fetching data from the
+				JSON data file to minimise the creation of redundant variables.
+				<br />
+				<br /> Figma design files were being used as a guide to laying out the
+				website.
+				<br />
+				<br /> The desktop navigation was implemented in JavaScript to save
+				myself the burden of copying/pasting the html on every page created.
+				<br />
+				<br />
+				That way, the menu appears on new pages automatically. The desktop
+				navigation was implemented using ES6 classes and private fields
 			</p>
 		),
 	},
