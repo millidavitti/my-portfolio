2e9df2c351409aea7559e4658cb2e60e0d83492c
Donald <83835207+millidavitti@users.noreply.github.com>
2022-09-30 18:15:22 +0100

    bio update

--- src/components/about/Me.jsx
+++ src/components/about/Me.jsx
@@ -7,48 +7,35 @@ export default function Me(props) {
 	return (
 		<section className={css.aboutMe}>
 			<div>
-				<img
-					className={css.avatar}
-					src={avatar}
-					alt='Donald Abua'
-				/>
+				<img className={css.avatar} src={avatar} alt='Donald Abua' />
 				<article>
 					<h2>Donald Abua</h2>
 					<p>
-						I can see you're a bit curious. Well, I am
-						motivated, task-oriented, efficient, and my
-						knowledge of the front-end web development
-						industry is comprehensive and current. My
-						enthusiasm and commitment to excellence has served
-						me well in learning the fundamentals and advanced
-						concepts in front edn development. I have
-						extensive experience with vanilla JavaScript,
-						having had the chance to significantly develop my
-						front-end development skills while working on
-						little side projects.
+						I can see you're a bit curious. Well, I am motivated, task-oriented,
+						efficient, and my knowledge of the front-end web development
+						industry is comprehensive and current. My enthusiasm and commitment
+						to excellence has served me well in learning the fundamental and
+						advanced concepts in front end development. I have extensive
+						experience with vanilla JavaScript, React UI library, a and have had
+						the chance to significantly develop my front-end development skills
+						while working on little side projects.
 						<br />
 						<br />
-						When I'm not thinking and writing code, I do what
-						I've always loved doing before I decided to start
-						a career in Front-end development i.e make music
-						on my laptop. To be precise I produce
-						instrumentals (Afro-beats). You can say I'm music
-						producer 😎.
+						When I'm not thinking and writing code, I do what I've always loved
+						doing before I decided to pursue a career in Front-end development
+						i.e make music on my laptop. To be precise I produce instrumentals,
+						😎.
 						<br />
 						<br />
-						Apart from making and grooving to my own
-						instrumentals, I consume any content on human
-						psychology I find interesting. Jordan B. Peterson
-						inspires me in that aspect. I also spending a lot
-						of time on Quora (answered a few simple questions
-						too)
+						Apart from making and grooving to my own instrumentals, I like to
+						consume content on human psychology I find interesting and lots of
+						Jordan B. Peterson. I also spend a lot of time on Quora (answered a
+						few simple questions too)
 						<br />
 						<br />
-						Lastly, I create graphics with Adobe Illustrator.
-						I usually create graffiti and head shot
-						illustrations. Once in a while I attempt
-						replicating logos. That's about it! Adios amigo(s)
-						😏
+						Lastly, I create graphics with Adobe Illustrator. I usually create
+						graffiti and head shot illustrations. Once in a while I attempt
+						replicating logos. That's about it! Adios amigo(s) 😏
 						<br />
 						<br />
 						<span>abuadonald@gmail.com</span>
