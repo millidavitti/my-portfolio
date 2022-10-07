7dc08a5922628b61a5b2c85fb322b4985d797854
Donald <83835207+millidavitti@users.noreply.github.com>
2022-10-07 16:36:31 +0100

    cv link update

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -4,39 +4,39 @@ import React from "react";
 import download from "../../assets/icons/download.png";
 
 export default function Header() {
-  return (
-    <header>
-      <div className='hero-content'>
-        <div className='hero-wrap'>
-          <div className=''>
-            <h2>
-              I'm <span>Donald Abua.</span>
-            </h2>
-            <h3>FrontEnd Developer!</h3>
-            <p>Welcome, a bit of tea and byte of bread?</p>
-          </div>
+	return (
+		<header>
+			<div className='hero-content'>
+				<div className='hero-wrap'>
+					<div className=''>
+						<h2>
+							I'm <span>Donald Abua.</span>
+						</h2>
+						<h3>FrontEnd Developer!</h3>
+						<p>Welcome, a bit of tea and byte of bread?</p>
+					</div>
 
-          <div className='buttons'>
-            <button>
-              <a href='#port'>View Portfolio</a>
-            </button>
+					<div className='buttons'>
+						<button>
+							<a href='#port'>View Portfolio</a>
+						</button>
 
-            <button>
-              <a
-                href='https://my.indeed.com/p/donalda-2wf5elc'
-                target='blank'
-                rel='noopener noreferrer'
-              >
-                {" "}
-              </a>
-              <span>
-                <img src={download} alt='download-icon'></img>
-              </span>
-              Download CV
-            </button>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+						<button>
+							<a
+								href='https://drive.google.com/file/d/1enYWXTcfF8UJT1YRlF5kSuOEityGCe8I/view?usp=sharing'
+								target='blank'
+								rel='noopener noreferrer'
+							>
+								{" "}
+							</a>
+							<span>
+								<img src={download} alt='download-icon'></img>
+							</span>
+							Download CV
+						</button>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 }
