e4701c60122b4ad01e0cf2ba3047116b745df0ee
Donald Abua <abuadonald@gmail.com>
2022-10-21 20:04:48 +0100

    new links

--- src/components/portfolio/ProjectInfo.jsx
+++ src/components/portfolio/ProjectInfo.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function ProjectInfo(props) {
 	const { css, title, img, text, link } = props;
 	return (
-		<div className={css.project}>
-			<a href={link} target='_blank' rel='noopener noreferrer'>
+		<a href={link} target='_blank' rel='noopener noreferrer'>
+			<div className={css.project}>
 				<h2>{title}</h2>
 				<img src={img} alt='roll' />
 				{text}
-			</a>
-		</div>
+			</div>
+		</a>
 	);
 }
